4c72a22fb81be2f9f649926bd7375b20de3a6311
alanandrade <alan.andradebispo@gmail.com>
2018-01-27 17:19:42 -0200

    adicionando formulario pelo codigo fonte, insercao css e if de page slug contato

--- wp-content/themes/malura/page.php
+++ wp-content/themes/malura/page.php
@@ -21,7 +21,28 @@
             }
         ?>
 
-    </article>
-</main>
+        <?php if(is_page('contato')) { ?>
 
-<?php get_footer(); ?>
+        <form>
+            <div class="form-name">
+                <label for=="form-nome">Nome:</label>
+                <input type="text" id="form-nome" placeholder="Seu nome aqui" name="form-nome">
+            </div>
+
+            <div class="form-email">
+                <labek for="form-email">Email:</label>
+                <input type="email" id="form-email" placeholder="email@exemplo.com.br" name="form-email">
+            </div>
+
+            <div class="form-mensagem">
+                <labek for="form-mensagem">Email:</label>
+                <textarea id="form-mensagem" name="form-email"></textarea>
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+        
+        <?php } ?>
+
+    </article>
+    <?php get_footer(); ?>
+</main>
