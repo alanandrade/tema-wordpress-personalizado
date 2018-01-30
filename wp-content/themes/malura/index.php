7c384ad2bcfa0d6fe567a69d345da56b4108e323
alanandrade <alan.andradebispo@gmail.com>
2018-01-30 16:46:35 -0200

    finalizacao tema wordpress base

--- wp-content/themes/malura/index.php
+++ wp-content/themes/malura/index.php
@@ -1,12 +1,46 @@
 <?php
+    $queryTaxonomy = array_key_exists('taxonomy', $_GET);
+    if( $queryTaxonomy && $_GET['taxonomy'] === '' ){
+        wp_redirect( home_url() );
+    }
     $css_especifico = 'index';
     require_once('header.php');  //Recupera o header para o Index
 ?>
 
 <main class="home-main">
-    <div class="container">    
+    <div class="container">
+
+        <?php $taxonomias = get_terms('localizacao'); ?>
+
+        <form class="busca-localizacao-form" method="GET" action="<?= bloginfo('url'); ?>">
+            <div class="taxonomy-select-wrapper">
+                <select name="taxonomy">
+                    <option value="">Todos os imóveis</option>
+                    <?php foreach($taxonomias as $taxonomia){ ?>
+                    <option value="<?= $taxonomia->slug; ?>"><?= $taxonomia->name; ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+            <button type="submit">Filtrar</button>
+        </form>    
+        
         <?php
-            $args = array( 'post_type' => 'imovel' );
+            if($queryTaxonomy){ //Se Existir a Busca, faça o code abaixo
+            $taxQuery = array(
+                    array(
+                        'taxonomy' => 'localizacao',
+                        'field' => 'slug',
+                        'terms' => $_GET['taxonomy']
+                    )
+                );
+            }
+
+            $args = array( 
+                'post_type' => 'imovel',
+                'tax_query' => $taxQuery
+            
+            );
+            
             $loop = new WP_Query( $args );
             if( $loop->have_posts() ) { 
         ?>
